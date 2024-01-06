bde70fe7ec4f8796fd7cbfd68ba7da1b485ef9ba
Aditi-Agrawal-07 <07aditiagrawal@gmail.com>
2024-01-06 16:42:13 +0530

    change the version of swagger

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require("passport")
 
 const options = {
     definition: {
-        openapi :' 3.0.0',
+        openapi :' 3.1.0',
         info: {
             title: "Job Portal Api",
             version: '1.0.0'
@@ -31,23 +31,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
 
-
-
-// Mongo session setup 
-// app.use(session({
-//     secret:process.env.JWT_SECRET,
-//     resave: false,
-//     saveUninitialized:true,
-//     store: MongoStore.create({
-//         mongoUrl: process.env.MONGODB_URI,
-//         collection: session
-//     }),
-//     cookie: { 
-//         maxAge:1000 *60 * 60 * 24
-//     }
-// }))
-// app.use(passport.initialize())
-// app.use(passport.session())
 
 // Import Utilities
 const  { database } = require("./utils") 
