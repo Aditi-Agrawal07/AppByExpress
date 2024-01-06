1d560960d0a88e98d35a3c0255cff889e7eb41b0
Aditi-Agrawal-04 <aditia.bca2022@ssism.org>
2024-01-06 11:36:55 +0530

    config

--- utils/passport.js
+++ utils/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const crypto = require('./crypto.utils')
-require('dotenv').config
+require('dotenv').config()
 const { Student, Recruiter, Teacher } = require('../Models');
 
 // local stratergy import
